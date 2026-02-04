424c23ce57be5eaf87a08ffd0f93ed3f797aebb4
blrxbadboy <rayenbounawara7@gmail.com>
2026-02-04 20:23:37 +0100

    problem

---
+++ control.js
@@ -0,0 +1,108 @@
+//ex 1
+function each(tab,func) {
+    for (var i = 0; i < tab.length; i++) {
+        func(tab[i])
+        
+    }
+    
+}
+//ex 2
+function sumSquares(numbers) {
+    var total=0
+    each(numbers,function(el) {
+        total+=el*el
+    })
+    return total
+}
+
+//ex 3
+function impairs(numbers) {
+    var result=[]
+    each(numbers,function(el) {
+        if(el%2!==0){
+        result.push(el)
+        }
+    })
+    return result
+}
+
+//ex4
+function sommeDeTousLesÉlémentsMultipliésParQuatre(numbers) {
+    var total=0
+    each(numbers,function(el) {
+        total+=el*4
+    })
+    return total
+}
+
+//ex 5
+function doublerTous(nombres) {
+    var result=[]
+    each(nombres,function(el) {
+        result.push(el*2)
+    })
+    return result
+}
+
+//ex 6
+function diviserTousParDeux(nombres) {
+    var result=[]
+    each(nombres,function(el){
+        result.push(el/2)
+    })
+    return result
+}
+
+//ex 7
+function mettreEnMajuscules(chaines) {
+    var result=[]
+    each(chaines,function(el){
+        result.push(el.toUpperCase());
+    })
+    return result
+}
+
+//ex 8
+function pairs(nombres) {
+    var result=[]
+    each(nombres,function(el) {
+        if (el%2===0){
+            result.push(el)
+        }
+    })
+    return result
+}
+
+//ex 9
+function multiplesDeTrois(nombres) {
+    var result=[]
+    each(nombres,function(el) {
+        if (el%3===0){
+            result.push(el)
+        }
+    })
+    return result
+}
+
+// ex 10
+function positifs(nombres) {
+    var result=[]
+    each(nombres,function(el){
+        if(el>0){
+            result.push(el)
+        }
+    })
+    return result
+}
+
+//ex 11
+function longueurPaire(chaines) {
+    var result=[]
+    each(chaines,function(el) {
+        if (el.length%2===0) {
+            result.push(el)
+        }
+    })
+    return result
+}
+//ex 012
